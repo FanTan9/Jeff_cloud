ad1fbbc80895e11e85ec65758a7cfc12286099eb
tangfan <824644610@qq.com>
2019-07-19 11:51:34 +0800

    //RedisUtils工具类以及StringUtils工具类

---
+++ boxServer/src/main/java/com/jeff/utill/StringUtils.java
@@ -0,0 +1,12 @@
+package com.jeff.utill;
+
+/**
+ * @Classname: StringUtils
+ * @PackageName: com.jeff.utill
+ * @Description: TODO
+ * @Date: 2019/7/19 11:50
+ * @Created by: tangfan
+ * @version: 1.0.0
+ */
+public class StringUtils extends org.springframework.util.StringUtils {
+}

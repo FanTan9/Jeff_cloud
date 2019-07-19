2d7ad1623b739ab5a884b387f0ed799b427b278a
tangfan <824644610@qq.com>
2019-07-19 10:46:49 +0800

    //修改controller名字

--- boxServer/src/main/java/com/jeff/controller/AdminBoxsController.java
+++ boxServer/src/main/java/com/jeff/controller/AdminBoxsController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version: 1.0.0
  */
 @RestController
-public class ScenePlayStatusController {
+public class AdminBoxsController {
 
 	@Autowired
 	private RedisTemplate redisTemplate;
